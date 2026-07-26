e8586d645e81bcf4cadf689526b89d62cd0e37fb
Subhrajitnaskar <subhrajitnaskar516@gmail.com>
2026-07-26 09:45:46 +0530

    now

--- routes/review.js
+++ routes/review.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
@@ -27,6 +27,7 @@ router.post(
     "/",
     validateReview,
     wrapAsync(async (req, res) => {
+        // console.log(req.params.id);
         let listing = await Listing.findById(req.params.id);
 
         let newReview = new Review(req.body.review);
