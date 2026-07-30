2a3605b16a496a0e05a026c07f8d11f99cefab6a
Subhrajitnaskar <subhrajitnaskar516@gmail.com>
2026-07-30 23:21:21 +0530

    now

--- routes/user.js
+++ routes/user.js
@@ -7,14 +7,18 @@ const { saveRedirectUrl } = require("../middleware.js");
 
 const userController = require("../controllers/users.js");
 
-router.get("/signup", userController.renderSignupForm);
+router
+ .route("/signup")
+  .get( userController.renderSignupForm)
+  .post( wrapAsync(userController.signup));
 
-router.post("/signup", wrapAsync(userController.signup));
+//router.get("/signup", userController.renderSignupForm);
 
-router.get("/login", userController.renderLoginForm);
+//router.post("/signup", wrapAsync(userController.signup));
 
-router.post(
-    "/login",
+router.route("/login")
+.get( userController.renderLoginForm)
+.post(
     saveRedirectUrl,
     passport.authenticate("local", {
         failureRedirect: "/login",
@@ -22,6 +26,19 @@ router.post(
     }),
    userController.login
 );
+
+
+// router.get("/login", userController.renderLoginForm);
+
+// router.post(
+//     "/login",
+//     saveRedirectUrl,
+//     passport.authenticate("local", {
+//         failureRedirect: "/login",
+//         failureFlash: true,
+//     }),
+//    userController.login
+// );
 
 router.get("/logout", userController.logout);
 
