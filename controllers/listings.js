87702703d9a7e7d5b7e7c394087c6e33b203ef7e
Subhrajitnaskar <subhrajitnaskar516@gmail.com>
2026-07-31 23:05:32 +0530

    now

--- controllers/listings.js
+++ controllers/listings.js
@@ -32,24 +32,16 @@ module.exports.showListing = async (req, res) => {
 
 module.exports.createListing = async (req, res) => {
 
-    // console.log(req.body);
-    // console.log(req.file);
+    let url = req.file.path;
+    let filename = req.file.filename;
+  
 
     const newListing = new Listing(req.body.listing);
-
     newListing.owner = req.user._id;
 
-    if (req.file) {
-        newListing.image = {
-            url: req.file.path,
-            filename: req.file.filename,
-        };
-    }
-
+    newListing.image = { url, filename };
     await newListing.save();
-
     req.flash("success", "New Listing Created Successfully!");
-
     res.redirect("/listings");
 };
 
