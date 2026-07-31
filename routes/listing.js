63aee36aca96bf93e14b40dcdd80920c19792c7e
Subhrajitnaskar <subhrajitnaskar516@gmail.com>
2026-07-31 13:09:28 +0530

    now

--- routes/listing.js
+++ routes/listing.js
@@ -7,17 +7,22 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 
 const listngController = require("../controllers/listings.js");
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router
 .route("/")
  .get(
    wrapAsync(listngController.index)
   )
- .post(
-   isLoggedIn,
-   validateListing,
-   wrapAsync(listngController.createListing)
-  );
+//  .post(
+//    isLoggedIn,
+//    validateListing,
+//    wrapAsync(listngController.createListing)
+//   );
+.post( upload.single('listing[image]'), (req,res) => {
+  res.send(req.file);
+});
 
   // New Route
 router.get("/new", isLoggedIn, listngController.renderNewFrom);
